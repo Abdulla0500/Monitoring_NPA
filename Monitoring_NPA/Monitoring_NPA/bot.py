42e368187f87c73135798fa748e957b6c800702d
Abdulla <abdula05mugi@gmail.com>
2026-02-26 16:56:46 +0300

    fff

--- Monitoring_NPA/Monitoring_NPA/bot.py
+++ Monitoring_NPA/Monitoring_NPA/bot.py
@@ -1246,7 +1246,7 @@ def main():
     scheduler = AsyncIOScheduler()
     scheduler.add_job(
              send_daily_notifications,
-             trigger=CronTrigger(minute='*'),
+             trigger=CronTrigger(hour="17" , minute='0'),
              args=[application],
              id='test_notifications',
              replace_existing=True
